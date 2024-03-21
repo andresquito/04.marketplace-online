29759f01e98e23534780c69a4f330ebf0cadcf2f
andresquito <ricardoandre1@hotmail.com>
2024-03-21 15:58:28 -0500

    Start

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import Sidebar from "./components/shared/Sidebar";
+import { FaUser, FaBars, FaTimes, FaSearch, FaAngleDown } from "react-icons/fa";
+
+function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [shorOrder, setShowOrder] = useState(false);
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+  return (
+    <div className="bg-[#262837]  w-full  min-h-screen">
+      <Sidebar showMenu={showMenu} />
+      {/* menu movil*/}
+      <nav className="bg-[#1F1D2B] lg:hidden  fixed w-20 right-4 bottom-4  text-3xl text-gray-400 py-2 px-8 flex items-center justify-center  ">
+        {/* <button className="p-2"><FaUser /></button>
+        <button className="p-2"><FaPlus /></button>
+        <button className="p-2"><FaChartPie /></button> */}
+        <button onClick={toggleMenu} className="text-white p-2 ml-auto">
+          {showMenu ? <FaTimes /> : <FaBars />}
+        </button>
+      </nav>
+      {/* home */}
+      <main className="lg:pl-28 grid grid-cols-1 lg-grid-cols-8 p-4">
+        <div className="lg:col-span-6">
+          {/* Header */}
+          <header className="">
+            {/* Title and search */}
+            <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6 ">
+              <h1 className="text-2xl text-gray-300">Andrés Yépez</h1>
+              <p className="text-gray-500">21 marzo 2022</p>
+            </div>
+            <form action="">
+              <div className="w-full relative">
+                <FaSearch className="absolute left-3 top-1/2 -traslate-y-1/2 text-gray-300" />
+                <input
+                  type="text"
+                  className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none"
+                  placeholder="Search"
+                />
+              </div>
+            </form>
+            {/* Tabs */}
+            <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6 lg:gap-8">
+              <a href="#" className="relative py-2 pr-4  text-[#ec7c6a] ">
+                Hot dishes
+              </a>
+              <a href="#" className="py-2 pr4">
+                Cold dishes
+              </a>
+              <a href="#" className="py-2 pr-4">
+                Soup
+              </a>
+              <a href="#" className="py-2">
+                Grill
+              </a>
+            </nav>
+          </header>
+          {/* Title content */}
+          <div className="flex items-center justify-between">
+            <h2 className="text-gray-300 text-xl">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <FaAngleDown /> Dine in
+            </button>
+          </div>
+          {/* Content */}
+          <div className="p-8 grid grid-cols-2">
+            {/* Card */}
+            <div>
+              <img src="plato.png" alt="" />
+            </div>
+            <div>
+              <img src="plato.png" alt="" />
+            </div>
+          </div>
+
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/shared/Sidebar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import {
+  FaHome,
+  FaPercentage,
+  FaChartPie,
+  FaBell,
+  FaRegEnvelope,
+  FaRegSun,
+  FaSignInAlt
+} from "react-icons/fa";
+
+const Sidebar = (props) => {
+  const {showMenu} = props;
+  return (
+    <div className={`bg-[#1F1D2B] fixed leg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6  rounded-tr-xl rounded-br-xl z-50 transition-all ${showMenu ? "left-0" :"-left-full"}`}>
+      <div>
+        <ul className="pl-4">
+          <li>
+            <h1 className="text-2xl text-gray-300 uppercase font-bold text-center my-5">
+              Logo
+            </h1>
+          </li>
+
+          <li className="bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl ">
+            <a
+              href="#"
+              className="bg-[#Ec7c6a] p-4 flex justify-center rounded-xl  text-white"
+            >
+              <FaHome className="text-2xl " />
+            </a>
+          </li>
+
+          <li className=" hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#Ec7c6a] p-4 flex justify-center rounded-xl  text-[#Ec7c6a] group-hover:text-white transition-colors"
+            >
+              <FaPercentage className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#Ec7c6a] p-4 flex justify-center rounded-xl  text-[#Ec7c6a] group-hover:text-white transition-colors"
+            >
+              <FaChartPie className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#Ec7c6a] p-4 flex justify-center rounded-xl  text-[#Ec7c6a] group-hover:text-white transition-colors"
+            >
+              <FaRegEnvelope className="text-2xl " />
+            </a>
+          </li>
+
+          <li className=" hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#Ec7c6a] p-4 flex justify-center rounded-xl  text-[#Ec7c6a] group-hover:text-white transition-colors"
+            >
+              <FaBell className="text-2xl " />
+            </a>
+          </li>
+          <li className=" hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#Ec7c6a] p-4 flex justify-center rounded-xl  text-[#Ec7c6a] group-hover:text-white transition-colors"
+            >
+              <FaRegSun className="text-2xl " />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="pl-4">
+          <li className=" hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#Ec7c6a] p-4 flex justify-center rounded-xl  text-[#Ec7c6a] group-hover:text-white transition-colors"
+            >
+              <FaSignInAlt className="text-2xl " />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
